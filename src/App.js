d0c9a84bd27c0e62efde7cbfc0d959b32e079b06
Taesub Yun <taesubyun@icloud.com>
2022-01-28 15:26:00 +0900

    초기 구조 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
 
 function App() {
+    const [count, setCount] = useState(100);
+
+    const handleCount = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div className='App'>
-            <h2>react-hooks-useEffect</h2>
+            <div>
+                <h2>react-hooks-useEffect</h2>
+            </div>
+            <div>
+                <button onClick={handleCount}>카운트버튼</button>
+                <span>카운트 : {count}</span>
+            </div>
         </div>
     );
 }
